32cc19448028fdec156cdc12b30f440095c9bc47
izak270 <58972658+izak270@users.noreply.github.com>
2020-05-05 15:09:41 +0300

    adding sendSlow function

--- email-server/send-emails.js
+++ email-server/send-emails.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer')
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
+    pool: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
@@ -17,17 +18,29 @@ const sendMail = (item) => {
     let mailOption = {
         from: process.env.EMAIL,
         to: item.email,
-        subject: `Hello ${item.firstName} my name is Itzhak hirschman and this is my rosome`,
-        text: item.lastName,
+        subject: `Hello ${item.firstName}-Itzhak Hirschman CV`,
+        text:
+            `My name is Itzhak Hirschman and I would like to inform you that this email was sent using a script I wrote (with node.js) 
+        that automatically pulled out your email address from my Linkedin account and sent you this mail.
+        You can take a look at the script in this link: 'https://github.com/izak270/sendEmail'
+
+        I'm looking for a career opportunity as a Junior backend or frontend developer.
+        I'll be grateful If you can assist me in distributing my resume to others who are looking for a creative and very passionate web 
+        developer.
+        
+        BR and stay safe,
+        Itzhak Hirschman.`,
         attachments: [
             {
                 path: process.env.FILEPATH
             }
         ]
     };
+
     transporter.sendMail(mailOption, function (err, data) {
         if (err) {
             console.log('error occurred', err);
+            process.exit(1);
         }
         else {
             sentEmails++
@@ -35,7 +48,15 @@ const sendMail = (item) => {
         }
     })
 }
-users.usersIfo.forEach(sendMail)
-
+let i = 0
+function sendSlow() {
+    console.log(users.usersIfo[i]);
+    // sendMail(users.usersIfo[i])
+    i++;
+    if (i < users.usersIfo.length) {
+        setTimeout(sendSlow, 5000);
+    }
+}
+sendSlow()
 
 
